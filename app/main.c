90a9930f70c1610a002c02408e04230b612715e5
Vinayaka <vinayaka.c@axiomconsult.com>
2015-09-24 16:38:52 +0530

    added blinky

--- app/main.c
+++ app/main.c
@@ -4,27 +4,64 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-void 
-prvSetupHardware( void )
+void prvSetupHardware( void )
 {
 	SystemInit();
-	SystemCoreClockUpdate();
+	
+	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;
+	SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK;
+
+    PORTB->PCR[18] = PORT_PCR_MUX(1);
+    PTB->PDDR |= (1 << 18);
+    PTB->PSOR |= (1 << 18);
+
+    PORTB->PCR[19] = PORT_PCR_MUX(1);
+    PTB->PDDR |= (1 << 19);
+    PTB->PSOR |= (1 << 19);
+
+    PORTD->PCR[1] = PORT_PCR_MUX(1);
+    PTD->PDDR |= (1 << 1);
+    PTD->PSOR |= (1 << 1);
 }
 
-void 
-taskA( void *pvParameters)
+void taskA( void *pvParameters)
 {
 	for(;;)
 	{
-
+		PTB->PTOR |= (1 << 18);
+		vTaskDelay(300);
 	}
 }
 
-int 
-main(void)
+void taskB( void *pvParameters)
+{
+	for(;;)
+	{
+		PTB->PTOR |= (1 << 19);
+		vTaskDelay(200);
+	}
+}
+
+void taskC( void *pvParameters)
+{
+	for(;;)
+	{
+		PTD->PTOR |= (1 << 1);
+		vTaskDelay(400);
+	}
+}
+
+void taskD( void *pvParameters)
+{
+	for(;;)
+	{
+		vTaskDelay(200);
+	}
+}
+
+int main(void)
 {
 	prvSetupHardware();
-
 	xTaskCreate( taskA, 
 				 "taskA", 
 				 configMINIMAL_STACK_SIZE, 
@@ -33,25 +70,48 @@ main(void)
 				 NULL 
 				);
 
+	xTaskCreate( taskB, 
+				 "taskB", 
+				 configMINIMAL_STACK_SIZE, 
+				 ( void * ) NULL, 
+				 4,
+				 NULL 
+				);
+
+	xTaskCreate( taskC, 
+				 "taskC", 
+				 configMINIMAL_STACK_SIZE, 
+				 ( void * ) NULL, 
+				 5,
+				 NULL 
+				);
+
+	xTaskCreate( taskD, 
+				 "taskD", 
+				 configMINIMAL_STACK_SIZE, 
+				 ( void * ) NULL, 
+				 6,
+				 NULL 
+				);
+
+
 	vTaskStartScheduler();
+	for(;;);
 	return 0;
 }
 
 
-void 
-vConfigureTimerForRunTimeStats( void )
+void vConfigureTimerForRunTimeStats( void )
 {
 
 }
 
-void 
-vApplicationTickHook( void )
+void vApplicationTickHook( void )
 {
 
 }
 
-void 
-vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
 {
 
 }
